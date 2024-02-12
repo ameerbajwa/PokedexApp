9cc5694d5dc5af836d8339ebfabfa23586c4f562
Ameer Bajwa <ameer.bajwa@revelsystems.com>
2024-02-12 06:34:16 -0800

    Creating networking layer

--- PokedexApp/PokedexApp/Networking/NetworkService.swift
+++ PokedexApp/PokedexApp/Networking/NetworkService.swift
@@ -10,22 +10,32 @@ import Foundation
 struct NetworkService {
     
     var session: URLSession!
+    var decoder: JSONDecoder!
     
-    init(urlSession: URLSession) {
+    init(urlSession: URLSession, jsonDecoder: JSONDecoder) {
         self.session = urlSession
+        self.decoder = jsonDecoder
     }
     
-    func callPokeAPI(with endpoint: Endpoint, by id: Int?) {
+    func callPokeAPI(with endpoint: Endpoint,
+                     by id: Int?,
+                     for model: Codable,
+                     completionHandler: @escaping (Result<Codable, Error>) -> Void) {
         let pokeAPIUrlString = generatePokeAPIUrl(with: endpoint, by: id)
         let pokeAPIUrl = URL(fileURLWithPath: pokeAPIUrlString)
         var pokeAPIUrlRequest = URLRequest(url: pokeAPIUrl)
         pokeAPIUrlRequest.httpMethod = HttpMethod.GET.rawValue
         
-//        let task = session.dataTask(with: pokeAPIUrlRequest) { pokeAPIData, pokeAPIUrlResponse, pokeAPIError in
-//            <#code#>
-//        }
+        let task = session.dataTask(with: pokeAPIUrlRequest) { pokeAPIData, pokeAPIUrlResponse, pokeAPIError in
+            guard let safePokeAPIData = pokeAPIData, pokeAPIError == nil else {
+                return
+            }
+            
+            var responseModel = try decoder.decode(model.self, from: safePokeAPIData)
+            completionHandler(.success(responseModel))
+        }
         
-//        task.resume()
+        task.resume()
     }
     
     func generatePokeAPIUrl(with endpoint: Endpoint, by id: Int?) -> String {
